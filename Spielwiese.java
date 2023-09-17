686814f186d42d28d225dec5df046023fcd4d16f
Till Ritter <till.ritter@outlook.de>
2023-09-17 12:51:58 +0200

    Hab Salami eingefügt

--- Spielwiese.java
+++ Spielwiese.java
@@ -12,7 +12,8 @@ class Spielwiese {
     public Spielwiese() {
         warenbestand = new ArrayList<Artikel>();
         warenbestand.add(new Artikel(1, "Müsli", 1.82, 3.99));    
-        warenbestand.add(new Artikel(1, "Hafermilch", 0.76, 1.39));    
+        warenbestand.add(new Artikel(2, "Hafermilch", 0.76, 1.39));    
+        warenbestand.add(new Artikel(3, "Salami", 3.99, 4.59));    
     }
 
     public static void main(String[] args) {
